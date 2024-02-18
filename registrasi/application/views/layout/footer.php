b9a7aef1a1f2094eb333dacbe3612cf5777cc448
DikaAntoniPutra <dikahanggara2@gmail.com>
2024-02-18 22:34:20 +0700

    dika

---
+++ registrasi/application/views/layout/footer.php
@@ -0,0 +1,12 @@
+<div class="flex-grow-1"></div>
+<div class="app-footer">
+    <div class="footer-bottom pt-3 d-flex flex-column flex-sm-row align-items-center">
+        <div class="d-flex align-items-center">
+            <img class="logo" src="<?= base_url().'dist-assets/'?>/images/logo.png" alt="">
+            <div class="ml-2">
+                <p class="m-0">&copy; 2018 Gull HTML</p>
+                <p class="m-0">All rights reserved</p>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ registrasi/application/views/layout/navigation.php
@@ -0,0 +1,7 @@
+<?php 
+    if ($this->session->userdata('auth')->id_role == '1' || $this->session->userdata('auth')->id_role == '2') {
+        $this->load->view('layout/navigation_admin') ;
+    } else {
+        $this->load->view('layout/navigation_common');
+    }
+?>

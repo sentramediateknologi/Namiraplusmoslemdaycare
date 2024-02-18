5827a9a860e4cefa8be34c07a9d2152ac42fb3ef
DikaAntoniPutra <dikahanggara2@gmail.com>
2024-02-18 23:20:26 +0700

    dika namira

--- registrasi/application/views/layout/navigation.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    if ($this->session->userdata('auth')->id_role == '1' || $this->session->userdata('auth')->id_role == '2') {
-        $this->load->view('layout/navigation_admin') ;
-    } else {
-        $this->load->view('layout/navigation_common');
-    }
-?>
